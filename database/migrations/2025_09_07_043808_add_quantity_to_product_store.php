d6070958430c6c85955928f23380315cd4d8634c
sothearythorng7 <sothearythorng7@gmail.com>
2025-09-07 06:40:08 +0200

    fix

---
+++ database/migrations/2025_09_07_043808_add_quantity_to_product_store.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('product_store', function (Blueprint $table) {
+            if (!Schema::hasColumn('product_store', 'stock_quantity')) {
+                $table->integer('stock_quantity')->default(0)->after('store_id');
+            }
+
+            if (!Schema::hasColumn('product_store', 'alert_stock_quantity')) {
+                $table->integer('alert_stock_quantity')->default(0)->after('stock_quantity');
+            }
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('product_store', function (Blueprint $table) {
+            $table->dropColumn(['stock_quantity', 'alert_stock_quantity']);
+        });
+    }
+};
